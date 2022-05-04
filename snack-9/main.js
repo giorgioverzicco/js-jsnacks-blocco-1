c8191c34d47a038b08460a1a8be67ba1b4c391a7
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-04 17:27:20 +0200

    switched to while

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,7 +1,7 @@
 const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const arr2 = [1, 2, 3, 4, 5, 6];
 
-for (let i = 0; arr1.length !== arr2.length; i++) {
+while (arr1.length !== arr2.length) {
   const rand = Math.floor(Math.random() * 100) + 1;
   if (arr1.length < arr2.length) {
     arr1.push(rand);
