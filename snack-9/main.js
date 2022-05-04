1a39546061c9aa8af847baa5be5f26b502424888
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-04 17:30:21 +0200

    refactor everything is random

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,5 +1,17 @@
-const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const arr2 = [1, 2, 3, 4, 5, 6];
+// const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arr2 = [1, 2, 3, 4, 5, 6];
+const arr1 = [];
+const arr2 = [];
+
+for (let i = 0; i < Math.floor(Math.random() * 100) + 10; i++) {
+  const rand = Math.floor(Math.random() * 100) + 1;
+  arr1.push(rand);
+}
+
+for (let i = 0; i < Math.floor(Math.random() * 100) + 10; i++) {
+  const rand = Math.floor(Math.random() * 100) + 1;
+  arr2.push(rand);
+}
 
 while (arr1.length !== arr2.length) {
   const rand = Math.floor(Math.random() * 100) + 1;
