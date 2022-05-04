7edf52e1bed990bc9583429ec12157b2c5c23ceb
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-04 12:33:03 +0200

    fix iteration count

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,5 +1,5 @@
 let list = [];
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 6; i++) {
   let userInput = Number(prompt("Inserisci un numero:"));
   alert(userInput % 2 ? `${userInput} è dispari` : `${userInput} è pari`);
 }
