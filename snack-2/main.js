414e1e8cb558e554d34e3f76d50fc9d13a9b5935
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-04 11:43:40 +0200

    add snack 2

---
+++ snack-2/main.js
@@ -0,0 +1,3 @@
+const firstWord = prompt("Scrivi una parola:");
+const secondWord = prompt("Scrivi un'altra parola:");
+alert(firstWord.length >= secondWord.length ? firstWord + " " + secondWord : secondWord + " " + firstWord);
