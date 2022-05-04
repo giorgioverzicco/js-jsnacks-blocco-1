d020cb8ec83bf1681e7600fe0857af72df2a1ab8
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-04 17:00:02 +0200

    refactor clean code

--- snack-7/main.js
+++ snack-7/main.js
@@ -6,6 +6,7 @@ for (let i = 0; i < firstNames.length; i++) {
   const randFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
   const randLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
   const fullName = randFirstName + " " + randLastName;
+
   if (!guests.includes(fullName)) {
     guests.push(fullName);
   }
